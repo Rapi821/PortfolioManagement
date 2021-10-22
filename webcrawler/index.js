1d81b5943b9274a23bb4b593e2f4010cb43117c3
Raphael Wolfsberger <wolfsberger.r03@htlwienwest.at>
2021-10-22 09:04:13 +0200

    mehrere aktien crawlen

--- webcrawler/index.js
+++ webcrawler/index.js
@@ -1,37 +1,60 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
 
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://www.finanzen.net/aktien/amazon-aktie');
-  //getIsin
-  const [el] = await page.$x(
-    '/html/body/div[2]/div[1]/div[2]/div[9]/div[1]/div[1]/div[2]/div[4]/div/span'
-  );
-  const isinData = await el.getProperty('textContent');
-  const isin = await isinData.jsonValue();
-  console.log(isin);
+const aktien = ['amazon', 'apple'];
+let json;
+let akObj = {
+  name: '',
+  isin: '',
+  wkn: '',
+  symbol: '',
+  kurs: 0,
+  time: 0,
+};
 
-  //get Kurs
-  const [ku] = await page.$x(
-    '/html/body/div[2]/div[1]/div[2]/div[9]/div[1]/div[1]/div[2]/div[1]/div[1]'
-  );
-  const kursData = await ku.getProperty('textContent');
-  const kurs = await kursData.jsonValue();
-  console.log(kurs);
+for (let elm of aktien) {
+  (async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(`https://www.finanzen.net/aktien/${elm}-aktie`);
 
-  // get Name
-  const [n] = await page.$x(
-    '/html/body/div[2]/div[1]/div[2]/div[9]/div[1]/div[1]/div[1]/h1'
-  );
-  const nameData = await n.getProperty('textContent');
-  const name = await nameData.jsonValue();
-  console.log(name);
+    //getIsin
+    const [el] = await page.$x(
+      '/html/body/div[2]/div[1]/div[2]/div[9]/div[1]/div[1]/div[2]/div[4]/div/span'
+    );
+    const isinData = await el.getProperty('textContent');
+    const isin = await isinData.jsonValue();
+    let info = isin.split(' / ');
+    let wkn = info[0].split(': ');
+    let is = info[1].split(': ');
+    let sym = info[2].split(': ');
 
-  getTime();
+    akObj.isin = is[1];
+    akObj.wkn = wkn[1];
+    akObj.symbol = sym[1];
 
-  browser.close();
-})();
+    //get Kurs
+    const [ku] = await page.$x(
+      '/html/body/div[2]/div[1]/div[2]/div[9]/div[1]/div[1]/div[2]/div[1]/div[1]'
+    );
+    const kursData = await ku.getProperty('textContent');
+    const kurs = await kursData.jsonValue();
+    akObj.kurs = kurs;
+
+    // get Name
+    const [n] = await page.$x(
+      '/html/body/div[2]/div[1]/div[2]/div[9]/div[1]/div[1]/div[1]/h1'
+    );
+    const nameData = await n.getProperty('textContent');
+    const name = await nameData.jsonValue();
+    akObj.name = name;
+
+    akObj.time = getTime();
+
+    console.log(akObj);
+    browser.close();
+  })();
+}
 
 function getTime() {
   let today = new Date();
@@ -40,5 +63,5 @@ function getTime() {
   let time =
     today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
   let dateTime = date + ' ' + time;
-  console.log(dateTime);
+  return dateTime;
 }
