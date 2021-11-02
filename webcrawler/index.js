c93d9409f93bbb86c120f7431bc6117dc8307cc3
Raphael Wolfsberger <wolfsberger.r03@htlwienwest.at>
2021-11-02 09:03:20 +0100

    aktien hinzugefügt

--- webcrawler/index.js
+++ webcrawler/index.js
@@ -2,8 +2,7 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 const CronJob = require('cron').CronJob;
 
-
-const aktien = ['amazon', 'apple', 'microsoft'];
+const aktien = ['amazon', 'apple', 'microsoft', 'alphabet'];
 
 let akObj = {
   name: '',
@@ -15,71 +14,72 @@ let akObj = {
   time: 0,
 };
 
-
 // Cron Jede Minute crawlen
-let job = new CronJob('* * * * *', function () {
-  for (let elm of aktien) {
-    (async () => {
-      const browser = await puppeteer.launch();
-      const page = await browser.newPage();
-      await page.goto(`https://www.finanzen.net/aktien/${elm}-aktie`);
+let job = new CronJob(
+  '* * * * *',
+  function () {
+    for (let elm of aktien) {
+      (async () => {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto(`https://www.finanzen.net/aktien/${elm}-aktie`);
 
-      // ISIN, WKN, Symbol holen
-      const [el] = await page.$x(
-        '/html/body/div[2]/div[1]/div[2]/div[9]/div[1]/div[1]/div[2]/div[4]/div/span'
-      );
-      const isinData = await el.getProperty('textContent');
-      const isin = await isinData.jsonValue();
-      let info = isin.split(' / ');
-      let wkn = info[0].split(': ');
-      let is = info[1].split(': ');
-      let sym = info[2].split(': ');
+        // ISIN, WKN, Symbol holen
+        const [el] = await page.$x(
+          '/html/body/div[2]/div[1]/div[2]/div[9]/div[1]/div[1]/div[2]/div[4]/div/span'
+        );
+        const isinData = await el.getProperty('textContent');
+        const isin = await isinData.jsonValue();
+        let info = isin.split(' / ');
+        let wkn = info[0].split(': ');
+        let is = info[1].split(': ');
+        let sym = info[2].split(': ');
 
-      akObj.isin = is[1];
-      akObj.wkn = wkn[1];
-      akObj.symbol = sym[1];
+        akObj.isin = is[1];
+        akObj.wkn = wkn[1];
+        akObj.symbol = sym[1];
 
-      // Derzeitigen Kurs bekommen
-      const [ku] = await page.$x(
-        '/html/body/div[2]/div[1]/div[2]/div[9]/div[1]/div[1]/div[2]/div[1]/div[1]'
-      );
-      const kursData = await ku.getProperty('textContent');
-      let kurs = await kursData.jsonValue();
-      kurs = kurs.split('E');
-      akObj.kurs = kurs[0];
-      akObj.waehrung = 'E'+ kurs[1];
+        // Derzeitigen Kurs bekommen
+        const [ku] = await page.$x(
+          '/html/body/div[2]/div[1]/div[2]/div[9]/div[1]/div[1]/div[2]/div[1]/div[1]'
+        );
+        const kursData = await ku.getProperty('textContent');
+        let kurs = await kursData.jsonValue();
+        kurs = kurs.split('E');
+        akObj.kurs = kurs[0];
+        akObj.waehrung = 'E' + kurs[1];
 
-      // Aktien namen bekommen
-      const [n] = await page.$x(
-        '/html/body/div[2]/div[1]/div[2]/div[9]/div[1]/div[1]/div[1]/h1'
-      );
-      // const n = await page.$eval(() =>
-      //   Array.from(
-      //     document.querySelectorAll('.quotebox'),
-      //     (element) => element.textContent
-      //   )
-      // );
-      // console.log(n);
-      const nameData = await n.getProperty('textContent');
-      let name = await nameData.jsonValue();
-      name = name.split(' ');
-      akObj.name = name[0];
+        // Aktien namen bekommen
+        const [n] = await page.$x(
+          '/html/body/div[2]/div[1]/div[2]/div[9]/div[1]/div[1]/div[1]/h1'
+        );
+        // const n = await page.$eval(() =>
+        //   Array.from(
+        //     document.querySelectorAll('.quotebox'),
+        //     (element) => element.textContent
+        //   )
+        // );
+        // console.log(n);
+        const nameData = await n.getProperty('textContent');
+        let name = await nameData.jsonValue();
+        name = name.split(' ');
+        akObj.name = name[0];
 
-      // Derzeitiges Datum & Zeit zum Objekt hinzufügen
-      akObj.time = getTime();
+        // Derzeitiges Datum & Zeit zum Objekt hinzufügen
+        akObj.time = getTime();
 
-      console.log(akObj);
+        console.log(akObj);
 
-      // fs.appendFileSync('./aktien.json', JSON.stringify(akObj));
+        // fs.appendFileSync('./aktien.json', JSON.stringify(akObj));
 
-      browser.close();
-    })();
-  }
-
-}, "Americas/Vancouver");
+        browser.close();
+      })();
+    }
+  },
+  'Americas/Vancouver'
+);
 
 job.start();
-
 
 // Function um Datum & Zeit zu bekommen
 function getTime() {
