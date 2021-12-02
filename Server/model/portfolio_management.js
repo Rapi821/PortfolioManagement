11ce7f27751f49bd46d37684d3d746851a68c822
xSabba <devall.s03@htlwienwest.at>
2021-12-02 18:58:46 +0100

    getCompetitionsByUser Route buy_price zu cash geändert. Damit es verständlicher ist

--- Server/model/portfolio_management.js
+++ Server/model/portfolio_management.js
@@ -6,7 +6,7 @@ const getUserByEmail = async (email) => (await query('SELECT * FROM users WHERE 
 //Neuen User registrieren
 const registerNewUser = async (newData) => (await query('INSERT INTO users (email, firstname, lastname, password) VALUES ($1, $2, $3, $4) returning *', [newData.email, newData.firstname, newData.lastname, newData.password])).rows;
 //Alle Competitions von einem User
-const getCompetitionsByUser = async (user_id) => (await query("select cm.member_id, c.title, (select sum(buy_price) as cash from competition_member_depot_lines where member_id= 1 and isin != '0000') as portfolio_value, cmdl.buy_price, (select sum(buy_price) from competition_member_depot_lines where member_id= 1) as total, c.active from competition_members cm JOIN competitions c on c.competition_id= cm.competition_id JOIN competition_member_depot_lines cmdl on c.competition_id = cmdl.competition_id where cm.user_id=$1 and cmdl.isin= '0000';", [user_id])).rows;
+const getCompetitionsByUser = async (user_id) => (await query("select cm.member_id, c.title, (select sum(buy_price) from competition_member_depot_lines where member_id= 1 and isin != '0000') as portfolio_value, cmdl.buy_price as cash, (select sum(buy_price) from competition_member_depot_lines where member_id= 1) as total, c.active from competition_members cm JOIN competitions c on c.competition_id= cm.competition_id JOIN competition_member_depot_lines cmdl on c.competition_id = cmdl.competition_id where cm.user_id=$1 and cmdl.isin= '0000';", [user_id])).rows;
 
 module.exports = {
   getUsers,
