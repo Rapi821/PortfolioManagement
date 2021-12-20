1b2fd7a85de8a1b700597f64309c9484ee6a827c
Raphael Wolfsberger <wolfsberger.r03@htlwienwest.at>
2021-12-20 10:57:45 +0100

    competitons dialog

--- web_client/src/views/MainMenu.vue
+++ web_client/src/views/MainMenu.vue
@@ -4,15 +4,58 @@
     <v-card>
       <v-card-title>Deine Competitions</v-card-title>
       <v-card-actions
-        ><v-btn>Competition Beitreten</v-btn>
-        <v-btn>Neue Competition</v-btn></v-card-actions
+        ><v-btn>Competition Beitreten</v-btn
+        ><v-btn @click="compCreate">Competition Erstellen</v-btn>
+        <v-dialog v-model="dialog" max-width="500px">
+          <v-card>
+            <v-card-title>
+              <span class="text-h5">Competition erstellen</span>
+            </v-card-title>
+
+            <v-card-text>
+              <v-container>
+                <v-row>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field
+                      v-model="competetion.title"
+                      label="Titel"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field
+                      v-model="competetion.starting_money"
+                      label="Startgeld"
+                    ></v-text-field> </v-col
+                  ><v-col cols="12" sm="6" md="4">
+                    <v-text-field
+                      v-model="competetion.end_date"
+                      label="End Datum"
+                    ></v-text-field> </v-col
+                ></v-row>
+              </v-container>
+            </v-card-text>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue darken-1" text @click="close">
+                Cancel
+              </v-btn>
+              <v-btn color="blue darken-1" text @click="createCompetition">
+                Create
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog></v-card-actions
       >
     </v-card>
+    <v-data-table :headers="headers" :items="competetions" class="elevation-1">
+    </v-data-table>
   </div>
 </template>
 
 <script>
 import TopBar from '../components/TopBar.vue';
+import server from '@/serverInterface';
 export default {
   components: {
     TopBar,
@@ -20,8 +63,56 @@ export default {
   props: {
     user_id: String,
   },
-  created() {
-   
+  methods: {
+    close() {
+      this.dialog = false;
+    },
+    compCreate() {
+      this.dialog = true;
+    },
+    async getComps() {
+      this.competetions = (
+        await server.get(
+          `http://localhost:3000/getCompetitions/${this.user_id}`
+        )
+      ).data;
+    },
+    async createCompetition() {
+      console.log(this.competetion);
+      await server.post(
+        `http://localhost:3000/createNewCompetition`,
+        this.competetion
+      );
+      this.close();
+      this.getComps();
+    },
+  },
+  data() {
+    return {
+      competetions: [],
+      dialog: false,
+      competetion: {
+        title: '',
+        starting_money: 0,
+        end_date: '',
+        user_id: this.user_id,
+      },
+      headers: [
+        {
+          text: 'titel',
+          align: 'start',
+          sortable: false,
+          value: 'title',
+        },
+        { text: 'Portfoliowert', value: 'portfolio_value' },
+        { text: 'Cash', value: 'cash' },
+        { text: 'Total', value: 'total' },
+        { text: 'Status', value: 'active' },
+      ],
+    };
+  },
+  async created() {
+    this.getComps();
   },
 };
 </script>
