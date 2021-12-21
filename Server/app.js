f5a8a2ff07afc3c28c5aa3a653b82fb17598a430
leogoldbacher <goldbacher.l02@htlwienwest.at>
2021-12-21 13:04:14 +0100

    frontend kleinichkeiten

--- Server/app.js
+++ Server/app.js
@@ -1,39 +1,39 @@
 /* eslint-disable import/order */
 /* eslint-disable no-unused-vars */
 /* eslint-disable global-require */
-const express = require('express');
-const morgan = require('morgan');
-const helmet = require('helmet');
-const path = require('path');
-const persons = require('./routes/portfolio_management');
-const { errorHandler, notFound } = require('./middleware/errorHandler');
-const session = require('express-session');
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const path = require("path");
+const persons = require("./routes/portfolio_management");
+const { errorHandler, notFound } = require("./middleware/errorHandler");
+const session = require("express-session");
 
-require('colors');
-require('dotenv').config();
+require("colors");
+require("dotenv").config();
 
 const app = express();
 
 // eslint-disable-next-line no-undef-init
 let sessionStore = undefined;
-const pgsession = require('connect-pg-simple')(session);
+const pgsession = require("connect-pg-simple")(session);
 
 // eslint-disable-next-line new-cap
 sessionStore = new pgsession({
-  pool: require('./db/index').pool,
+  pool: require("./db/index").pool,
   // Connection Pool
-  tableName: 'user_sessions',
+  tableName: "user_sessions",
 });
 
 const sess = session({
   store: sessionStore,
-  secret: 'awrecnireooahuo121345678765432345678',
+  secret: "awrecnireooahuo121345678765432345678",
   resave: false,
   saveUninitialized: false,
   cookie: {
     maxAge: 30 * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    secure: 'auto', // wenn https verwendet wird auf true setzen
+    secure: "auto", // wenn https verwendet wird auf true setzen
   },
 });
 
@@ -43,28 +43,28 @@ const isDevelopment = true; // Restservice on LocalHost 3000 and Client on Local
 app.use(helmet());
 if (isDevelopment) {
   corsOption = {
-    origin: 'http://localhost:8081',
+    origin: "http://localhost:8080",
     credentials: true,
   };
-  const cors = require('cors');
+  const cors = require("cors");
   app.use(cors(corsOption));
 }
 app.use(sess);
 
 function userCheck(req, res, next) {
   if (!req.session.db_user_id) {
-    res.status(401).json({ error: 'Error' });
+    res.status(401).json({ error: "Error" });
     return;
   }
   next();
 }
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.use(express.json());
-app.use('/', persons);
+app.use("/", persons);
 
 app.use(errorHandler);
 app.use(notFound);
