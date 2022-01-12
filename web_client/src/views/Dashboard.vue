5d66e28e1c87026245f06ea13431721eb1317c7a
leogoldbacher <goldbacher.l02@htlwienwest.at>
2022-01-12 19:21:02 +0100

    Dashboard Frontend weitergemacht bisschen

--- web_client/src/views/Dashboard.vue
+++ web_client/src/views/Dashboard.vue
@@ -1,36 +1,56 @@
 <template>
+  <div>
+    <TopBar />
 
-<div>
-   <v-row no-gutters> 
-      <v-col
-        v-for="n in 3"
-        :key="n"
-        cols="12"
-        sm="4"
-      >
-      </v-col>
-      </v-row>
-  <v-card
-    class="mx-auto"
-    max-width="344"
-  >
-  
-    <v-list-item three-line>
-      <v-list-item-content>
-        <div class="text-overline mb-4">
+    <div class="d-flex">
+      <div class="d-none d-xl-flex b"></div>
+      <div class="mar">
+        <div class="z text-h5">
           Portfolio Wert
         </div>
-        <v-list-item-title class="text-h5 mb-1 mt-n6">
-          5774€
-        </v-list-item-title>
-        <div class="text-overline mb-4">
-          Portfolio Wert
-        </div>
-        <v-list-item-title class="text-h5 mb-1 mt-n6">
-          5774€
-        </v-list-item-title>
-      </v-list-item-content>
-    </v-list-item>
-  </v-card>
-</div>
+        <v-card class="" max-width="200">
+          <v-list-item three-line>
+            <v-list-item-content>
+              <div class=" text-overline mb-4">
+                Portfolio Wert
+              </div>
+              <v-list-item-title class="text-h6 mb-1 mt-n6">
+                5774€
+              </v-list-item-title>
+              <div class="text-overline mb-4">
+                Portfolio Wert
+              </div>
+              <v-list-item-title class="text-h6 mb-1 mt-n6">
+                5774€
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-card>
+      </div>
+    </div>
+  </div>
 </template>
+<script>
+import TopBar from "../components/TopBar.vue";
+
+export default {
+  components: {
+    TopBar,
+  },
+};
+</script>
+<style>
+.z {
+  margin-top: 150px; /* Nur Vorübergehend! */
+}
+.b {
+  width: 300px; /* entfernung von linkem bildschirmrand */
+  height: 100%;
+}
+.bod {
+  height: 100%;
+}
+.mar {
+  margin-left: 80px;
+}
+</style>
