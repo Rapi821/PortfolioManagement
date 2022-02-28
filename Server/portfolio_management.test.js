c5c0023d62f3b7f09a6b71511341f545b40b5ec0
ValentinSz <szwajda.v03@htlwienwest.at>
2022-02-28 13:44:10 +0100

    Get Users fertig und Create user

--- Server/portfolio_management.test.js
+++ Server/portfolio_management.test.js
@@ -22,5 +22,27 @@ describe('test people REST Service', () => {
     const res = await request.get('/users');
     console.log('UserTest');
     expect(res.status).toBe(200);
+    expect(res.body).toContainEqual({
+      email: 'alfred.reisenberger@htlwienwest.at',
+      firstname: 'Alfred',
+      lastname: 'Reisenberger',
+      password: '1Rrj?xKZL',
+      user_id: 3,
+    });
+  });
+
+  it('Create,Get,Update,Delete User', async () => {
+    const res = await request.post('/user/createNewOne').send({
+      email: 'szwajda.v03@htlwienwest.at',
+      firstname: 'Valentin',
+      lastname: 'Szwajda',
+      password: '1?Ä.*t<',
+      user_id: '9',
+    });
+    const iduser = res.body.user_id;
+    console.log(res.body.user_id);
+    expect(res.status).toBe(200);
+    // const resget = await request.get(`/users/${iduser}`);
+    // expect(resget.status).toBe(200);
   });
 });
