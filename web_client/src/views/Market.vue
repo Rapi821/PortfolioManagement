ce7180a421651711ab13f17302179b8345a92112
Leo Goldbacher <62808215+leogoldbacher@users.noreply.github.com>
2022-04-02 02:02:56 +0200

    Update Market.vue

--- web_client/src/views/Market.vue
+++ web_client/src/views/Market.vue
@@ -3,41 +3,64 @@
     <TopBarMarket :comp_id="comp_id" />
     <!-- <div class="d-none d-xl-flex a"></div> -->
     <!-- <v-spacer></v-spacer> -->
-    <v-container class="fill-height " fluid>
+    <v-container class="fill-height" fluid>
       <v-row align="center" justify="center">
         <v-col cols="12" sm="8">
           <v-data-table
-            dense
             :loading="loading"
             :headers="headers"
             :items="akData"
             :items-per-page="10"
-            class="elevation-1"
+            class="elevation-0"
           >
             <!-- Detailpage der einzelnen Aktien -->
-
-            <template v-slot:[`item.actions`]="{ item }">
-              <v-btn
-                :to="`/detailAk/${item.isin}/${comp_id}`"
-                small
-                plain
-                class="primary  mt-2 mb-2"
-                >Details</v-btn
-              >
-              <v-btn
-                @click="openSellDialog(item)"
-                small
-                plain
-                class="primary ml-2 mt-2 mb-2 me-2"
-                >Handeln</v-btn
-              >
+            <template v-slot:item="{ item }">
+              <tr>
+                <td>
+                  {{ item.name }}
+                </td>
+                <td>
+                  {{ item.isin }}
+                </td>
+                <td>
+                  {{ item.wkn }}
+                </td>
+                <td>{{ item.kurs }}</td>
+                <td>
+                  <v-sparkline
+                    line-width="1"
+                    :smooth="false"
+                    :value="item.graphvalues"
+                  ></v-sparkline>
+                </td>
+                <td>
+                  <v-btn
+                    :to="`/detailAk/${item.isin}/${comp_id}`"
+                    small
+                    plain
+                    class="primary mt-2 mb-2"
+                    >Details</v-btn
+                  >
+                  <v-btn
+                    @click="openSellDialog(item)"
+                    small
+                    plain
+                    class="primary ml-2 mt-2 mb-2 me-2"
+                    >Handeln</v-btn
+                  >
+                </td>
+              </tr>
             </template>
           </v-data-table>
         </v-col>
       </v-row>
     </v-container>
-
-    
+    <v-sparkline
+      line-width="1"
+      :smooth="false"
+      :value="this.allakKurs[0].werte"
+      auto-draw
+    ></v-sparkline>
     <!-- <v-spacer></v-spacer> -->
     <!-- <div class="d-none d-xl-flex a"></div> -->
   </div>
@@ -46,6 +69,7 @@
 <script>
 import axios from "axios";
 import TopBarMarket from "../components/TopBarMarket.vue";
+// import server from "@/serverInterface";
 export default {
   name: "Market",
   components: { TopBarMarket },
@@ -60,7 +84,9 @@ export default {
       akInfo: [],
       akKurs: [],
       akData: [],
-      
+      allakKurs: [],
+      hilfsarr: [],
+      hilfsarr2: [],
       headers: [
         {
           text: "Name",
@@ -71,8 +97,8 @@ export default {
         { text: "ISIN", value: "isin", sortable: false },
         { text: "WKN", value: "wkn", sortable: false },
         { text: "Kurs", value: "kurs" },
+        { text: "", value: "graphvalues" },
         { text: "Aktionen", value: "actions" },
-        { text: "", value: "verkaufen" },
       ],
     };
   },
@@ -80,12 +106,25 @@ export default {
     createAktie() {
       // Funktion die alle Aktein mit deren Kruse bekommt
       // Für Datatable werden Manuell Objekte für die einzelnen Aktein erstellt
+      let counter = 0;
       for (let elm of this.akInfo) {
-        let aktie = { name: elm.title, isin: elm.isin, wkn: elm.wkn, kurs: 1 };
+        console.log(counter);
+        let aktie = {
+          name: elm.title,
+          isin: elm.isin,
+          wkn: elm.wkn,
+          kurs: 1,
+          graphvalues: this.allakKurs[counter].werte,
+        };
+
+        console.log("test 1");
+        // console.log();
         this.akData.push(aktie);
+
+        counter++;
       }
       for (let el of this.akData) {
-        console.log(el);
+        // console.log(el);
         let wert = this.akKurs.find((e) => e.isin == el.isin);
         if (wert == undefined) {
           wert = { wert: 1 };
@@ -95,7 +134,71 @@ export default {
 
       // console.log(this.akData);
     },
-    
+    async getKurs() {
+      const today = new Date();
+      // const yesterday = today.getDate() - 31;
+      let lastMonth;
+      if (today.getMonth() + 1 == 1) {
+        lastMonth = 12;
+        let lastYear = today.getFullYear() - 1;
+        const yesterdayDate =
+          lastYear + "-" + lastMonth + "-" + today.getDate();
+        this.datum = yesterdayDate;
+        // console.log(yesterdayDate);
+        let counterid = 0;
+        for (let elem of this.akInfo) {
+          console.log(elem.isin);
+          let lol = (
+            await axios.get(
+              `https://heroku-porftolio-crawler.herokuapp.com/kursByTime/${elem.isin}?date=${yesterdayDate}`
+            )
+          ).data;
+          this.hilfsarr = [];
+          for (let elle of lol) {
+            this.hilfsarr.push(elle.wert);
+          }
+          let aktie = {
+            id: counterid,
+            werte: this.hilfsarr,
+          };
+          this.allakKurs.push(aktie);
+          counterid++;
+          // console.log(lol.filter((elem) => elem.wert));
+        }
+      } else {
+        const yesterdayDate =
+          today.getFullYear() + "-" + today.getMonth() + "-" + today.getDate();
+        // console.log(yesterdayDate);
+        let counterid = 0;
+        for (let elem of this.akInfo) {
+          console.log(elem.isin);
+          let lol = (
+            await axios.get(
+              `https://heroku-porftolio-crawler.herokuapp.com/kursByTime/${elem.isin}?date=${yesterdayDate}`
+            )
+          ).data;
+          this.hilfsarr = [];
+          for (let elle of lol) {
+            this.hilfsarr.push(elle.wert);
+          }
+          this.hilfsarr2 = [];
+          for (let i = 0; i < this.hilfsarr.length; i = i + 100) {
+            this.hilfsarr2.push(this.hilfsarr[i]);
+          }
+          let aktie = {
+            id: counterid,
+            werte: this.hilfsarr2,
+          };
+
+          this.allakKurs.push(aktie);
+          counterid++;
+          // console.log(lol.filter((elem) => elem.wert));
+        }
+      }
+
+      // console.log(test);
+      // console.log(this.akKurs);
+    },
   },
   async created() {
     this.loading = true;
@@ -105,7 +208,12 @@ export default {
     this.akKurs = (
       await axios.get("https://heroku-porftolio-crawler.herokuapp.com/akKurs")
     ).data;
+    // console.log(this.akKurs);
+
+    await this.getKurs();
     this.createAktie();
+    console.log(this.akInfo);
+    console.log(this.allakKurs);
     this.loading = false;
   },
 };
